205eb95a90b45fae88cc91434e0ae44570a0d184
haruki114114 <ktc25a31l0007@edu.kyoto-tech.ac.jp>
2025-10-18 11:39:32 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PokemonList from './pages/PokemonList';
 import PokemonDetail from './pages/PokemonDetail';
+import Header from './components/Header';
 import './App.css';
 
 // QueryClientのインスタンスを作成
@@ -19,9 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <div className="min-h-screen bg-gray-100">
-          <header className="bg-blue-600 text-white p-4">
-            <h1 className="text-2xl font-bold text-center">ポケモン図鑑</h1>
-          </header>
+          <Header />
           <main>
             <Routes>
               <Route path="/" element={<PokemonList />} />
