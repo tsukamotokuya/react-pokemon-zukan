cf1d3931f455ae484ffa1ddb21299bd5a0f074ef
murata-ktc <ktc25a31e0016@edu.kyoto-tech.ac.jp>
2025-11-15 11:21:53 +0900

    tailwindcssをプロジェクトに追加し、Viteの設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
